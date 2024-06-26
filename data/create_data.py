49ff8c0ab27f5bce2784c598b4a9fe9b5c000406
Faseel <faseelm98@gmail.com>
2024-06-26 10:41:29 +0200

    added graphs

--- data/create_data.py
+++ data/create_data.py
@@ -1,9 +1,14 @@
 import networkx as nx
 import random
+import json
 
 
 def generate_graph(num_nodes: int):
-
+    """
+    Generates a GNR (Growing network with reduction)
+    or GNC (Growing network with copying)
+    Graph will have arg "num_nodes" number of nodes
+    """
     redirection_probability = random.uniform(0, 0.3)
     graph_generator = [
         lambda: nx.gnr_graph(num_nodes, redirection_probability),
@@ -13,6 +18,9 @@ def generate_graph(num_nodes: int):
 
 
 def modify_graph_to_task_graph(graph: nx.DiGraph):
+    """
+    Add Task information as node and edge attributes to the arg "graph"
+    """
     require_generate_range = (1, 10)
 
     for node in graph.nodes:
@@ -60,6 +68,18 @@ def modify_graph_to_task_graph(graph: nx.DiGraph):
     return graph
 
 
+def save_graph_to_json(graph: nx.DiGraph, filename: str):
+    data = nx.node_link_data(graph)
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+def load_graph_from_json(filename: str):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    return nx.node_link_graph(data)
+
+
 def visualize_modified_graph(graph: nx.DiGraph):
     import matplotlib.pyplot as plt
     import networkx as nx
@@ -81,7 +101,7 @@ def visualize_modified_graph(graph: nx.DiGraph):
     )
 
     custom_labels = {
-        node: f"{node}\n(G: {graph.nodes[node].get('generate', 'N/A')})"
+        node: f"{node}\nG: {graph.nodes[node].get('generate', 'N/A')}"
         for node in graph.nodes
     }
     nx.draw_networkx_labels(graph, pos, labels=custom_labels)
@@ -91,13 +111,58 @@ def visualize_modified_graph(graph: nx.DiGraph):
 
     plt.show()
 
+def test_function(num_nodes: int):
+    graph = generate_graph(num_nodes)
+    modified_graph = modify_graph_to_task_graph(graph)
+    visualize_modified_graph(modified_graph)
+
+    save_graph_to_json(modified_graph, "test_task_graph.json")
+    loaded_graph = load_graph_from_json("test_task_graph.json")
+    visualize_modified_graph(loaded_graph)
+
+def generate_n_graphs(count: int): 
+    for i in range(count):
+        random_num_nodes = random.randint(2, 6)
+        graph = generate_graph(random_num_nodes)
+        modified_graph = modify_graph_to_task_graph(graph)
+
+        save_graph_to_json(modified_graph, f"pe_task_graphs/task_graph_{i}.json")
+
 
 if __name__ == "__main__":
 
-    import matplotlib.pyplot as plt
+    import argparse
 
-    # num of nodes range from 2 to 6
-    number_of_nodes = 2
-    graph = generate_graph(number_of_nodes)
-    modified_graph = modify_graph_to_task_graph(graph)
-    visualize_modified_graph(modified_graph)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--num_nodes",
+        type=int,
+        default=4,
+        help="Number of nodes in the generated graph",
+    )
+    parser.add_argument(
+        "--test", 
+        action="store_true",
+        help="Run the test function"
+    )
+    parser.add_argument(
+        "--generate", 
+        action="store_true",
+        help="Generate"
+    )
+    parser.add_argument(
+        "--gen_count", 
+        type=int,
+        default=100,
+        help="Number of graphs to generate"
+    )
+
+    args = parser.parse_args()
+    print(f"Number of nodes is {args.num_nodes}, testing is {args.test}, generating is {args.generate}, gen_count is {args.gen_count}")
+
+    if args.test:
+        test_function(args.num_nodes)
+
+    if args.generate:
+        generate_n_graphs(args.gen_count)
+
